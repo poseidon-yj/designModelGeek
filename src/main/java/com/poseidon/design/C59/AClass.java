57eebebae22cb196d1e0189d9f349359cc483583
poseidon_yj <poseidon_yj@163.com>
2022-10-26 20:31:18 +0800

    回调CallBack和模版模式

---
+++ src/main/java/com/poseidon/design/C59/AClass.java
@@ -0,0 +1,13 @@
+package com.poseidon.design.C59;
+
+public class AClass {
+    public static void main(String[] args) {
+        BClass b = new BClass();
+        b.process(new ICallBack() { //回调对象
+            @Override
+            public void methodToCallback() {
+                System.out.println("Call back me.");
+            }
+        });
+    }
+}
---
+++ src/main/java/com/poseidon/design/C59/BClass.java
@@ -0,0 +1,9 @@
+package com.poseidon.design.C59;
+
+public class BClass {
+    public void process(ICallBack callback) {
+        //...
+        callback.methodToCallback();
+        //...
+    }
+}
